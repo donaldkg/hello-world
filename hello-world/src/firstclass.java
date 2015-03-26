a782b44e3871b34d88a7fb0642fe6377cce28547
dddkeung <dkeung@msn.com>
2015-03-26 20:56:01 +0800

    Create firstclass.java

---
+++ hello-world/src/firstclass.java
@@ -0,0 +1,11 @@
+import java.util.Calendar;
+public class firstclass {
+public static void main(String[] argv)
+{
+System.out.println("Hello!!!!");
+System.out.println("Good Bye");
+System.out.println("I am Stefan Lazar");
+System.out.println("my changes here");
+// Hello, here is a fake conflict.
+}
+}
